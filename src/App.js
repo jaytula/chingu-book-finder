10bd551494e5ee1172765b8cc67cfcef2a3ea9de
Nicole Freed <nicole.freed@gmail.com>
2019-02-22 15:41:26 -0800

    import and render Search and BookList components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Search from './components/Search';
+import BookList from './components/BookList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Search></Search>
+        <BookList></BookList>
       </div>
     );
   }
