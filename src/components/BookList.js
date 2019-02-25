4d940d309755f15a6f03963dba1e34800759c2f3
Nicole Freed <nicole.freed@gmail.com>
2019-02-25 12:36:54 -0800

    map through data; reassign values to shorten them

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardTitle, CardSubtitle, CardBody, Button } from 'reactstrap';
 
 class BookList extends Component {
+
   render() {
+    const data = this.props.data;
+    const bookData = data.map(book => {
+      return({
+        id: book.id,
+        thumbnail: book.volumeInfo.imageLinks.thumbnail || null,
+        title: book.volumeInfo.title,
+        subtitle: book.volumeInfo.subtitle || null,
+        authors: book.volumeInfo.authors,
+        publisher: book.volumeInfo.publisher,
+        pageCount: book.volumeInfo.pageCount,
+        averageRating: book.volumeInfo.averageRating || 'No ratings yet'
+      });
+    });
+
     return (
       <div>
       </div>
