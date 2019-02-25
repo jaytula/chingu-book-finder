9707838e6198d7a207978db3ac845ac0ce66796f
Nicole Freed <nicole.freed@gmail.com>
2019-02-25 12:37:35 -0800

    map through data; render individual bookList items

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -18,6 +18,22 @@ class BookList extends Component {
       });
     });
 
+    const bookList = bookData.map(bookItem => {
+      return(
+          <Card key={bookItem.id} width="100%">
+            <CardImg src={bookItem.thumbnail} alt={bookItem.title} className="card-img"/>
+            <CardBody>
+              <CardTitle><h4>{bookItem.title}: {bookItem.subtitle}</h4></CardTitle>
+              <CardSubtitle><h5>{bookItem.authors}</h5></CardSubtitle>
+              <p><small>{bookItem.publisher}</small></p>
+              <p>{bookItem.pageCount} pages</p>
+              <p>Average Rating: {bookItem.averageRating}</p>
+              <Button className="btn-success">See More</Button>
+            </CardBody>
+          </Card>
+      );
+    });
+
     return (
       <div>
       </div>
