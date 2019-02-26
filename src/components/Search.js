e56241e3577e3599c79c0c57791651d1061cfffe
Nicole Freed <nicole.freed@gmail.com>
2019-02-26 13:25:45 -0800

    create search form with handler functions passed as props

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
 
 class Search extends Component {
+  
   render() {
     return (
-      <div>
-      </div>
+      <Form onSubmit={this.props.handleSearch}>
+        <Form.Input
+          fluid
+          size="big"
+          action={{ icon: "search" }}
+          placeholder="Search for a title, author, keyword, or ISBN"
+          onChange={this.props.handleChange}
+        />
+      </Form>
     );
   }
 }
