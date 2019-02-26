14320ddbfff4f9629a672315cb8d74e87a0b7321
Nicole Freed <nicole.freed@gmail.com>
2019-02-26 13:31:53 -0800

    refactor handleSearch() method & move out of componentDidMount()

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ class App extends Component {
 
     this.state = {
       searchTerm: '',
+      submittedSearchTerm: '',
       data: []
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
-  async componentDidMount() {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=unbearable+lightness+of+being&key=${API_KEY}`);
-    // console.log(response);
   handleChange(event) {
     this.setState({
       searchTerm: event.target.value
@@ -26,10 +25,22 @@ class App extends Component {
     console.log('searchTerm:', this.state.searchTerm);
   }
 
+  handleSearch = async () => {
+    // update search term
+    const { searchTerm } = this.state;
+    await this.setState({
+      submittedSearchTerm: searchTerm
+    });
+    console.log('submittedSearchTerm:', this.state.submittedSearchTerm);
+
+    // make API call to Google Books
+    const { submittedSearchTerm } = this.state;
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${submittedSearchTerm}&key=${API_KEY}`);
+
     if (response.ok) {
       const json = await response.json();
       console.log(json);
-      this.setState({
+      await this.setState({
         data: json.items
       });
       const data = this.state.data;
