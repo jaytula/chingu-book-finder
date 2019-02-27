8724a249444fc3f0142871f254fcb47a1773e37f
Jay T <jaytula@gmail.com>
2019-02-27 01:20:24 +0000

    Added a Modal button for each bookItem

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,38 +1,54 @@
-import React, { Component } from 'react';
-import { Grid, Card, Image, Button } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Grid, Card, Image, Button, Modal} from 'semantic-ui-react';
 
 class BookList extends Component {
-
   render() {
     const data = this.props.data;
     const bookData = data.map(book => {
-      return({
+      return {
         id: book.id,
         thumbnail: book.volumeInfo.imageLinks.thumbnail || null,
         title: book.volumeInfo.title,
-        subtitle: book.volumeInfo.subtitle ? ': ' + book.volumeInfo.subtitle : '',
+        subtitle: book.volumeInfo.subtitle
+          ? ': ' + book.volumeInfo.subtitle
+          : '',
         authors: book.volumeInfo.authors,
         publisher: book.volumeInfo.publisher,
         pageCount: book.volumeInfo.pageCount || '?',
-        averageRating: book.volumeInfo.averageRating || 'No ratings yet'
-      });
+        averageRating: book.volumeInfo.averageRating || 'No ratings yet',
+      };
     });
 
     const bookList = bookData.map(bookItem => {
-      return(
+      return (
         <Grid.Column key={bookItem.id}>
-            <Card>
-              <Image fluid src={bookItem.thumbnail} alt={bookItem.title} className="thumbnail"/>
-              <Card.Content>
-                <Card.Header>{bookItem.title}{bookItem.subtitle}</Card.Header>
-                <Card.Meta className="authors">{bookItem.authors}</Card.Meta>
-                <Card.Meta className="publisher">{bookItem.publisher}</Card.Meta>
-                <p>{bookItem.pageCount} pages</p>
-                <p>Average Rating: {bookItem.averageRating}</p>
-                <Card.Description></Card.Description>
-              <Button primary>See More</Button>
-              </Card.Content>
-            </Card>
+          <Card>
+            <Image
+              fluid
+              src={bookItem.thumbnail}
+              alt={bookItem.title}
+              className="thumbnail"
+            />
+            <Card.Content>
+              <Card.Header>
+                {bookItem.title}
+                {bookItem.subtitle}
+              </Card.Header>
+              <Card.Meta className="authors">{bookItem.authors}</Card.Meta>
+              <Card.Meta className="publisher">{bookItem.publisher}</Card.Meta>
+              <p>{bookItem.pageCount} pages</p>
+              <p>Average Rating: {bookItem.averageRating}</p>
+              <Card.Description />
+              <Modal trigger={<Button>See More...</Button>}>
+                <Modal.Header>Example Component</Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                    <p>{bookItem.title}</p>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
+            </Card.Content>
+          </Card>
         </Grid.Column>
       );
     });
